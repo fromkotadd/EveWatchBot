bfcd44cedf567cf4e9844d5baebff11222cba417
Evgeniy <kotadd2@gmail.com>
2022-11-06 18:32:31 +0200

    Init

---
+++ config.py
@@ -0,0 +1,3 @@
+TOKEN = 'MTAzODgyMTg3MzE1NDI2MTExNg.GzpeiM.FtmSACMmdkyyaL71IdI0zX1xneAwImg3ENt5Vs'
+
+__url_from_bot_invite = 'https://discord.com/api/oauth2/authorize?client_id=1038821873154261116&permissions=47104&scope=bot'
---
+++ main.py
@@ -0,0 +1,42 @@
+from config import TOKEN
+import cv2
+import mss
+import numpy as np
+import time
+import json
+
+
+
+sct = mss.mss()
+last_time = time.time()
+
+with open('mouse_poss\mouse_poss.json') as file:
+    parse_area = json.load(file)
+
+while True:
+    if time.time() - last_time < 2:
+        continue
+
+    img = np.asarray(sct.grab(parse_area))
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    lower_red = np.array([281, 90, 46])  # диапазон цвета HSV
+    upper_red = np.array([328, 90, 46])
+    mask0 = cv2.inRange(hsv, lower_red, upper_red)
+
+    lower_red = np.array([170, 50, 50])
+    upper_red = np.array([180, 255, 255])
+    mask1 = cv2.inRange(hsv, lower_red, upper_red)
+
+    mask = mask0 + mask1
+
+    hasRed = np.sum(mask)
+    if hasRed > 0:
+        print("ENEMY IN THE HOME")
+        time.sleep(1)
+        pass
+    else:
+        print("RED NOT detected!")
+        time.sleep(0.3)
+        print("New search...")
+        last_time = time.time()
