a9afb644de2488e529637035e6fbc0f1bd3bdf06
kotadd)noote_book <kotadd2@gmail.com>
2022-11-07 20:37:50 +0200

    cv2ParseModule.py обавленна логика удаления скрина и обработки исключения

--- eveDiscordBot.py
+++ eveDiscordBot.py
@@ -1,1 +1,18 @@
 from config import TOKEN
+import discord
+import asyncio
+import asyncpraw
+import os
+
+bot = discord.Client(intents=discord.Intents.default())
+ID_CHANNEL = 975788405004832771
+
+
+
+channel = bot.get_channel(975788405004832771)
+
+# channel.send(discord.Object(id='975788405004832771'), 'hello')
+channel.send(TOKEN)
+
+
+bot.run(token=TOKEN)
