ba9e6d197a339ba1372ae257f89d8472a0570ebf
kotadd)noote_book <kotadd2@gmail.com>
2022-11-07 20:38:08 +0200

    cv2ParseModule.py обавленна логика удаления скрина и обработки исключения

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-TOKEN = 'MTAzODgyMTg3MzE1NDI2MTExNg.GzpeiM.FtmSACMmdkyyaL71IdI0zX1xneAwImg3ENt5Vs'
+TOKEN = 'MTAzODgyMTg3MzE1NDI2MTExNg.GzTSKl.yZm2O8QLTKfazuUjOwYJzvwuFBXNR0I0mjlzOo'
 
 __url_from_bot_invite = 'https://discord.com/api/oauth2/authorize?client_id=1038821873154261116&permissions=47104&scope=bot'
