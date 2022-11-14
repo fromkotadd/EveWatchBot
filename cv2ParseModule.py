bdf659728d4938dc142966c5a32addedc7c52089
Evgeniy <kotadd2@gmail.com>
2022-11-14 11:29:35 +0200

    alpha work ver.

--- cv2ParseModule.py
+++ cv2ParseModule.py
@@ -7,46 +7,47 @@ import time
 import json
 import os
 
+def cv2ParseModule():
+    sct = mss.mss()
+    last_time = time.time()
 
-sct = mss.mss()
-last_time = time.time()
+    path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'object_create.jpg')
+    with open('mouse_poss\mouse_poss.json') as file:
+        parse_area = json.load(file)
+    while True:
+        if time.time() - last_time < 2:
+            continue
 
-path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'object_create.jpg')
+        img = np.asarray(sct.grab(parse_area))
+        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-with open('mouse_poss\mouse_poss.json') as file:
-    parse_area = json.load(file)
+        lower_red = np.array([281, 90, 46])  # диапазон цвета HSV
+        upper_red = np.array([328, 90, 46])
+        mask0 = cv2.inRange(hsv, lower_red, upper_red)
 
-while True:
-    if time.time() - last_time < 2:
-        continue
+        lower_red = np.array([170, 50, 50])
+        upper_red = np.array([180, 255, 255])
+        mask1 = cv2.inRange(hsv, lower_red, upper_red)
 
-    img = np.asarray(sct.grab(parse_area))
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        mask = mask0 + mask1
 
-    lower_red = np.array([281, 90, 46])  # диапазон цвета HSV
-    upper_red = np.array([328, 90, 46])
-    mask0 = cv2.inRange(hsv, lower_red, upper_red)
+        hasRed = np.sum(mask)
+        if hasRed > 0:
+            print("ENEMY IN THE HOME")
+            sct.shot(output=f'object_create.jpg')
+            time.sleep(3)
+            pass
+        else:
 
-    lower_red = np.array([170, 50, 50])
-    upper_red = np.array([180, 255, 255])
-    mask1 = cv2.inRange(hsv, lower_red, upper_red)
+            try:
+                os.remove(path)
+            except (FileNotFoundError, PermissionError):
+                pass
 
-    mask = mask0 + mask1
+            print("RED NOT detected!")
+            time.sleep(0.3)
+            print("New search...")
+            last_time = time.time()
 
-    hasRed = np.sum(mask)
-    if hasRed > 0:
-        print("ENEMY IN THE HOME")
-        time.sleep(1)
-        sct.shot(output=f'object_create.jpg')
-        pass
-    else:
-
-        try:
-            os.remove(path)
-        except FileNotFoundError as EX:
-            print(EX)
-
-        print("RED NOT detected!")
-        time.sleep(0.3)
-        print("New search...")
-        last_time = time.time()
+if __name__ == '__main__':
+    cv2ParseModule()
