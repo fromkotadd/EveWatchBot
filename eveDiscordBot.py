3a63a2a94ace50b084c47313ac9da01851f76c9a
Evgeniy <kotadd2@gmail.com>
2022-11-14 11:29:43 +0200

    alpha work ver.

--- eveDiscordBot.py
+++ eveDiscordBot.py
@@ -1,18 +1,43 @@
-from config import TOKEN
-import discord
 import asyncio
-import asyncpraw
+import config
+import discord
+from discord.ext import commands
+import datetime
 import os
-
-bot = discord.Client(intents=discord.Intents.default())
-ID_CHANNEL = 975788405004832771
-
-
-
-channel = bot.get_channel(975788405004832771)
-
-# channel.send(discord.Object(id='975788405004832771'), 'hello')
-channel.send(TOKEN)
+import json
+import requests
+import glob
+import random
 
 
-bot.run(token=TOKEN)
+
+
+
+intents = discord.Intents.all()
+# intents.members = True
+bot = commands.Bot(command_prefix='!', intents=intents) #инициализируем бота с префиксом '!'
+TIMEOUT = 30
+ID_CHANNEL = 975788405004832770
+
+@bot.event
+async def on_ready():
+    channel = bot.get_channel(ID_CHANNEL)
+    while True:
+        try:
+            path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'object_create.jpg')
+            if path:
+                time_now = datetime.datetime.today().strftime("%H:%M:%S")
+                file = discord.File(f'C:\pythonProject\EveWatchBot\object_create.jpg', filename=f'object_create.jpg')
+                emned = discord.Embed(color=0xff9900, title=f'time: {time_now}')
+                emned.set_image(url=f"attachment://object_create.jpg")
+                print('screen download to chanel')
+                await channel.send(file=file, embed=emned)
+            else:
+                continue
+        except (FileNotFoundError, discord.errors.HTTPException) as EX:
+            print(EX)
+            continue
+
+        await asyncio.sleep(TIMEOUT)
+
+bot.run(config.TOKEN)
