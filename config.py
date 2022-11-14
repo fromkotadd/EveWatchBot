cf7658bce8cbdc6320c7de82f4603f1a920a3807
Evgeniy <kotadd2@gmail.com>
2022-11-14 11:18:27 +0200

    rename main file to cv2ParseModule

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-TOKEN = 'MTAzODgyMTg3MzE1NDI2MTExNg.GzTSKl.yZm2O8QLTKfazuUjOwYJzvwuFBXNR0I0mjlzOo'
+TOKEN = 'MTA0MTAyNDY2NDIwNzYzNDQ3Mg.GPME2P.6kF8X77JXd3TFd8su5XCzYZQSKoSNVRLpuIsec'
 
 __url_from_bot_invite = 'https://discord.com/api/oauth2/authorize?client_id=1038821873154261116&permissions=47104&scope=bot'
