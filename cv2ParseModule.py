33a9b68f871d0903779729a7b969ffef6d2c2937
Evgeniy <kotadd2@gmail.com>
2022-11-17 02:44:01 +0200

    бардак тестовый с прямоугольниками

--- cv2ParseModule.py
+++ cv2ParseModule.py
@@ -32,23 +32,45 @@ def cv2ParseModule():
         mask = mask0 + mask1
 
         hasRed = np.sum(mask)
-        # cv2.imshow("window_name", mask)
-        # cv2.waitKey(0)
-        if hasRed > 10:
-            print("ENEMY IN THE HOME")
-            sct.shot(output=f'object_create.jpg')
-            time.sleep(3)
-            pass
-        else:
-            try:
-                os.remove(path)
-            except (FileNotFoundError, PermissionError):
-                pass
+        cv2.imshow("window_name", mask)
+        cv2.waitKey()
+        cv2.destroyAllWindows()
+        # hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HS V)  # меняем цветовую модель с BGR на HSV
+        # thresh = cv2.inRange(hsv, hsv_min, hsv_max)  # применяем цветовой фильтр
+        contours0, hierarchy = cv2.findContours(mask.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-            print("RED NOT detected!")
-            time.sleep(0.3)
-            print("New search...")
-            last_time = time.time()
+        # перебираем все найденные контуры в цикле
+        for cnt in contours0:
+            rect = cv2.minAreaRect(cnt)  # пытаемся вписать прямоугольник
+            print(f'пытаемся вписать прямоугольник -{bool(rect)}')
+            box = cv2.boxPoints(rect)  # поиск четырех вершин прямоугольника
+
+            box = np.int0(box)  # округление координат
+            area = int(rect[1][0] * rect[1][1])  # вычисление площади
+            if area > 500:
+                print('TRUE')
+                cv2.drawContours(img, [box], 0, (255, 0, 0), 2)
+            # cv2.drawContours(mask, [box], 0, (170, 55, 55), 2)  # рисуем прямоугольник
+
+                cv2.imshow('contours', img)  # вывод обработанного кадра в окно
+
+                cv2.waitKey()
+                cv2.destroyAllWindows()
+        # if hasRed > 10:
+        #     print("ENEMY IN THE HOME")
+        #     sct.shot(output=f'object_create.jpg')
+        #     time.sleep(3)
+        #     pass
+        # else:
+        #     try:
+        #         os.remove(path)
+        #     except (FileNotFoundError, PermissionError):
+        #         pass
+        #
+        #     print("RED NOT detected!")
+        #     time.sleep(0.3)
+        #     print("New search...")
+        #     last_time = time.time()
 
 if __name__ == '__main__':
     cv2ParseModule()
