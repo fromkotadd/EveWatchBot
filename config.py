9ee14567970d1d62491ff5d0d4f5544bc5d3cb56
Evgeniy <kotadd2@gmail.com>
2022-12-01 13:03:19 +0200

    beta

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
-TOKEN = 'MTA0MTAyNDY2NDIwNzYzNDQ3Mg.GPME2P.6kF8X77JXd3TFd8su5XCzYZQSKoSNVRLpuIsec'
 
 __url_from_bot_invite = 'https://discord.com/api/oauth2/authorize?client_id=1038821873154261116&permissions=47104&scope=bot'
 
+config = {'TOKEN': 'MTA0MTAyNDY2NDIwNzYzNDQ3Mg.GPME2P.6kF8X77JXd3TFd8su5XCzYZQSKoSNVRLpuIsec',
+		  'ID_CHANNEL': 1047819504568258570,
+		  'PATH_PYTESSERACT': 'C:/Program Files/Tesseract-OCR/tesseract.exe',
+		  'TIMEOUTH' : 10,
+
+		  }
